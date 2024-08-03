fc9c3ec560c75a56084ff468a3cd6cb8aa924584
Llewelyn-Smit <111749335+Llewelyn-Smit@users.noreply.github.com>
2024-08-03 16:43:17 +0200

    PrimalSimplex Fixes

---
+++ WindowsFormsApp1/LoadDataFromTextFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class LoadDataFromTextFile
+    {
+      
+    }
+}
